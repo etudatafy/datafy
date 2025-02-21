b3f8ba7dfe5f3854158789870d4b5bd518831d7f
Doga Dalgali <d.dalgali@etu.edu.tr>
2025-02-21 21:19:35 +0300

    index.js some changtes reverted

--- frontend/aiwave-nextjs-app-route/app/signin/index.js
+++ frontend/aiwave-nextjs-app-route/app/signin/index.js
@@ -1,10 +1,13 @@
 "use client";
 import { useState } from "react";
 import { loginUser } from "../api";
+
 import Image from "next/image";
 import Link from "next/link";
 
 import logo from "../../public/images/logo/logo.png";
+import userImg from "../../public/images/team/team-02sm.jpg";
+import brandImg from "../../public/images/brand/brand-t.png";
 import google from "../../public/images/sign-up/google.png";
 import facebook from "../../public/images/sign-up/facebook.png";
 
@@ -43,8 +46,40 @@ const SigninPage = () => {
                   </div>
                   <div className="signup-box-bottom">
                     <div className="signup-box-content">
+                      <div className="social-btn-grp">
+                        <a className="btn-default btn-border" href="#">
+                          <span className="icon-left">
+                            <Image
+                              src={google}
+                              width={18}
+                              height={18}
+                              alt="Google Icon"
+                            />
+                          </span>
+                          Login with Google
+                        </a>
+                        <a className="btn-default btn-border" href="#">
+                          <span className="icon-left">
+                            <Image
+                              src={facebook}
+                              width={18}
+                              height={18}
+                              alt="Facebook Icon"
+                            />
+                          </span>
+                          Login with Facebook
+                        </a>
+                      </div>
+                      <div className="text-social-area">
+                        <hr />
+                        <span>Or continue with</span>
+                        <hr />
+                      </div>
                       <form onSubmit={handleLogin}>
                         <div className="input-section mail-section">
+                          <div className="icon">
+                            <i className="fa-sharp fa-regular fa-envelope"></i>
+                          </div>
                           <input
                             type="email"
                             placeholder="Enter email address"
@@ -54,6 +89,9 @@ const SigninPage = () => {
                           />
                         </div>
                         <div className="input-section password-section">
+                          <div className="icon">
+                            <i className="fa-sharp fa-regular fa-lock"></i>
+                          </div>
                           <input
                             type="password"
                             placeholder="Password"
@@ -63,17 +101,81 @@ const SigninPage = () => {
                           />
                         </div>
                         {error && <p className="error-message">{error}</p>}
+                        <div className="forget-text">
+                          <a className="btn-read-more" href="#">
+                            <span>Forgot password</span>
+                          </a>
+                        </div>
                         <button type="submit" className="btn-default">
                           Sign In
                         </button>
                       </form>
+                    </div>
+                    <div className="signup-box-footer">
+                      <div className="bottom-text">
+                        Don't have an account?{" "}
+                        <a className="btn-read-more ml--5" href="/signup">
+                          <span>Sign Up</span>
+                        </a>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
               <div className="col-lg-6 right-wrapper">
                 <div className="client-feedback-area">
-                  <p>AI destekli eğitim sistemimize hoş geldiniz!</p>
+                  <div className="single-feedback">
+                    <div className="inner">
+                      <div className="meta-img-section">
+                        <a className="image" href="#">
+                          <Image
+                            src={userImg}
+                            width={93}
+                            height={93}
+                            alt="User Image"
+                          />
+                        </a>
+                      </div>
+                      <div className="rating">
+                        <a href="#rating">
+                          <i className="fa-sharp fa-solid fa-star"></i>
+                        </a>
+                        <a href="#rating">
+                          <i className="fa-sharp fa-solid fa-star"></i>
+                        </a>
+                        <a href="#rating">
+                          <i className="fa-sharp fa-solid fa-star"></i>
+                        </a>
+                        <a href="#rating">
+                          <i className="fa-sharp fa-solid fa-star"></i>
+                        </a>
+                        <a href="#rating">
+                          <i className="fa-sharp fa-solid fa-star"></i>
+                        </a>
+                      </div>
+                      <div className="content">
+                        <p className="description">
+                          Rainbow-Themes is now a crucial component of our work!
+                          We made it simple to collaborate across departments by
+                          grouping our work
+                        </p>
+                        <div className="bottom-content">
+                          <div className="meta-info-section">
+                            <h4 className="title-text mb--0">Guy Hawkins</h4>
+                            <p className="desc mb--20">Nursing Assistant</p>
+                            <div className="desc-img">
+                              <Image
+                                src={brandImg}
+                                width={83}
+                                height={23}
+                                alt="Brand Image"
+                              />
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
